6083828a7de5d825d603074afbea20d08e2ec50d
Akash Agarwal <aksalive@live.com>
2023-04-07 18:55:24 -0700

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,104 +1,53 @@
-const cells = document.querySelectorAll('.cell');
-const turnIndicator = document.getElementById('turn-indicator');
-const newGameButton = document.getElementById('new-game');
+const getEmptyCells = () => Array.from(cells).filter(cell => !cell.textContent);
 
-let currentPlayer = 'X';
-let moves = 0;
-let gameOver = false;
-
-const winPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const switchPlayer = () => {
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  turnIndicator.textContent = `Player ${currentPlayer}'s turn`;
-};
-
-const updateScoreboard = () => {
-  const scoreX = document.getElementById('score-x');
-  const scoreO = document.getElementById('score-o');
-  scoreX.textContent = wins['X'];
-  scoreO.textContent = wins['O'];
-};
-
-const wins = { X: 0, O: 0 };
-updateScoreboard();
-
-const checkWinner = () => {
+const findWinningMove = (player) => {
   for (const pattern of winPatterns) {
     const [a, b, c] = pattern;
-    if (
-      cells[a].textContent &&
-      cells[a].textContent === cells[b].textContent &&
-      cells[a].textContent === cells[c].textContent
-    ) {
-      wins[currentPlayer]++;
-      updateScoreboard();
-      gameOver = true;
-      alert(`Player ${currentPlayer} wins!`);
-      break;
+    const patternValues = [cells[a].textContent, cells[b].textContent, cells[c].textContent];
+    const emptyIndex = patternValues.indexOf('');
+    if (emptyIndex > -1 && patternValues.filter(val => val === player).length === 2) {
+      return pattern[emptyIndex];
     }
   }
-
-  if (!gameOver && moves === 9) {
-    gameOver = true;
-    alert("It's a draw!");
-  }
+  return -1;
 };
 
-const makeMove = (cell) => {
-  if (gameOver || cell.textContent) {
-    return;
-  }
+const getCenterCell = () => (cells[4].textContent ? null : 4);
 
-  cell.textContent = currentPlayer;
-  moves++;
-
-  checkWinner();
-
-  if (!gameOver) {
-    switchPlayer();
-    if (currentPlayer === 'O') {
-      makeAutoMove();
-    }
-  }
+const getCornerCell = () => {
+  const cornerIndices = [0, 2, 6, 8];
+  const availableCorners = cornerIndices.filter(index => !cells[index].textContent);
+  return availableCorners.length ? availableCorners[Math.floor(Math.random() * availableCorners.length)] : null;
 };
 
 const makeAutoMove = () => {
-  let emptyCells = Array.from(cells).filter(cell => !cell.textContent);
+  const winningMove = findWinningMove('O');
+  if (winningMove > -1) {
+    makeMove(cells[winningMove]);
+    return;
+  }
+
+  const blockingMove = findWinningMove('X');
+  if (blockingMove > -1) {
+    makeMove(cells[blockingMove]);
+    return;
+  }
+
+  const centerCell = getCenterCell();
+  if (centerCell !== null) {
+    makeMove(cells[centerCell]);
+    return;
+  }
+
+  const cornerCell = getCornerCell();
+  if (cornerCell !== null) {
+    makeMove(cells[cornerCell]);
+    return;
+  }
+
+  const emptyCells = getEmptyCells();
   if (emptyCells.length > 0) {
     const randomIndex = Math.floor(Math.random() * emptyCells.length);
     makeMove(emptyCells[randomIndex]);
   }
 };
-
-cells.forEach((cell) => {
-  cell.addEventListener('click', (event) => {
-    if (currentPlayer === 'X') {
-      makeMove(event.target);
-    }
-  });
-});
-
-const resetGame = () => {
-  cells.forEach((cell) => {
-    cell.textContent = '';
-  });
-
-  currentPlayer = 'X';
-  moves = 0;
-  gameOver = false;
-
-  turnIndicator.textContent = `Player ${currentPlayer}'s turn`;
-};
-
-newGameButton.addEventListener('click', resetGame);
-
