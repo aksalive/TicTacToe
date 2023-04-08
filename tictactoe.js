9e8f916a8239727a756860763df2e347ba30c768
Akash Agarwal <aksalive@live.com>
2023-04-07 21:52:36 -0700

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,72 +1,127 @@
 const cells = document.querySelectorAll('.cell');
-const turnIndicator = document.getElementById('turn-indicator');
-const newGameButton = document.getElementById('new-game');
-let currentPlayer = 'X';
-let moves = 0;
-let gameOver = false;
+const turnIndicator = document.querySelector('.turn-indicator');
+const newGameButton = document.querySelector('.new-game');
+const scoreboardX = document.getElementById('score-x');
+const scoreboardO = document.getElementById('score-o');
+let board;
+let currentPlayer;
+let gameEnded;
 
-const winPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const switchPlayer = () => {
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+function startNewGame() {
+  board = ['', '', '', '', '', '', '', '', ''];
+  currentPlayer = 'X';
+  gameEnded = false;
   turnIndicator.textContent = `Player ${currentPlayer}'s turn`;
-};
 
-const updateScoreboard = () => {
-  const scoreX = document.getElementById('score-x');
-  const scoreO = document.getElementById('score-o');
-  scoreX.textContent = wins['X'];
-  scoreO.textContent = wins['O'];
-};
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].textContent = '';
+  }
+}
 
-const wins = { X: 0, O: 0 };
-updateScoreboard();
+function checkWin() {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal wins
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical wins
+    [0, 4, 8], [2, 4, 6] // Diagonal wins
+  ];
 
-const checkWinner = () => {
   for (const pattern of winPatterns) {
-    const [a, b, c] = pattern;
-    if (
-      cells[a].textContent &&
-      cells[a].textContent === cells[b].textContent &&
-      cells[a].textContent === cells[c].textContent
-    ) {
-      wins[currentPlayer]++;
-      updateScoreboard();
-      gameOver = true;
-      alert(`Player ${currentPlayer} wins!`);
-      break;
+    if (board[pattern[0]] && board[pattern[0]] === board[pattern[1]] && board[pattern[0]] === board[pattern[2]]) {
+      return true;
     }
   }
 
-  if (!gameOver && moves === 9) {
-    gameOver = true;
-    alert("It's a draw!");
-  }
-};
-
-const resetGame = () => {
-  for (const cell of cells) {
-    cell.textContent = '';
-  }
-  currentPlayer = 'X';
-  moves = 0;
-  gameOver = false;
-  turnIndicator.textContent = `Player ${currentPlayer}'s turn`;
-};
-
-newGameButton.addEventListener('click', resetGame);
-
-for (const cell of cells) {
-  cell.addEventListener('click', (event) => {
-    makeMove(event.target);
-  });
+  return false;
 }
+
+function checkDraw() {
+  return board.every(cell => cell !== '');
+}
+
+function endGame() {
+  gameEnded = true;
+
+  if (checkWin()) {
+    turnIndicator.textContent = `Player ${currentPlayer} wins!`;
+    if (currentPlayer === 'X') {
+      scoreboardX.textContent = Number(scoreboardX.textContent) + 1;
+    } else {
+      scoreboardO.textContent = Number(scoreboardO.textContent) + 1;
+    }
+  } else if (checkDraw()) {
+    turnIndicator.textContent = 'It\'s a draw!';
+  }
+}
+
+function cellClicked(index) {
+  if (gameEnded || board[index] !== '') return;
+
+  board[index] = currentPlayer;
+  cells[index].textContent = currentPlayer;
+
+  if (checkWin() || checkDraw()) {
+    endGame();
+  } else {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    turnIndicator.textContent = `Player ${currentPlayer}'s turn`;
+    if (currentPlayer === 'O') {
+      makeBestMove();
+    }
+  }
+}
+
+cells.forEach((cell, index) => {
+  cell.addEventListener('click', () => {
+    cellClicked(index);
+  });
+});
+
+newGameButton.addEventListener('click', startNewGame);
+
+function minimax(board, depth, isMaximizing) {
+  if (checkWin()) {
+    return isMaximizing ? -1 : 1;
+  }
+
+  if (checkDraw()) {
+    return 0;
+  }
+
+  const evaluateMoves = (move) => {
+    const newBoard = [...board];
+    newBoard[move] = isMaximizing ? 'O' : 'X';
+    return minimax(newBoard, depth + 1, !isMaximizing);
+  };
+
+  const availableMoves = board.reduce((acc, _, index) => board[index] === '' ? [...acc, index] : acc, []);
+  const scores
+  const scores = availableMoves.map(evaluateMoves);
+  return isMaximizing ? Math.max(...scores) - depth : Math.min(...scores) + depth;
+}
+
+function findBestMove(board) {
+  let bestMove = -1;
+  let bestScore = -Infinity;
+
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      const newBoard = [...board];
+      newBoard[i] = 'O';
+      const moveScore = minimax(newBoard, 0, false);
+
+      if (moveScore > bestScore) {
+        bestScore = moveScore;
+        bestMove = i;
+      }
+    }
+  }
+
+  return bestMove;
+}
+
+function makeBestMove() {
+  const bestMove = findBestMove(board);
+  cellClicked(bestMove);
+}
+
+startNewGame();
